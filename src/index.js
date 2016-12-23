01d87bdfbe900ca10912ac0798a1f7e3763a97a2
kongou-ae <y.matsumoto.ae@gmail.com>
2016-12-23 15:29:17 +0000

    fixed codeclimate

--- src/index.js
+++ src/index.js
@@ -19,11 +19,11 @@ module.exports = function(context, options = {}) {
                     
                     // 「等」か「など」が使われているか確認する
                     tokens.forEach(function(token,j){
-                        if ( token.surface_form == '等' && token.reading == 'トウ'){
+                        if ( token.surface_form === '等' && token.reading === 'トウ'){
                             firstFlag = true
                             position = token.word_position
                         }                         
-                        if ( token.surface_form == 'など' && token.reading == 'ナド'){
+                        if ( token.surface_form === 'など' && token.reading === 'ナド'){
                             firstFlag = true
                             position = token.word_position
                         }                         
@@ -32,16 +32,16 @@ module.exports = function(context, options = {}) {
                     // 使われていたら、その前に「や」があるかチェック
                     if (firstFlag == true){
                         tokens.forEach(function(token,k){
-                            if ( token.surface_form == 'や' && token.pos_detail_1 == '並立助詞'){
+                            if ( token.surface_form === 'や' && token.pos_detail_1 === '並立助詞'){
                                 secondFlag++
                             }
-                            if ( token.surface_form == 'と' && token.pos_detail_1 == '並立助詞'){
+                            if ( token.surface_form === 'と' && token.pos_detail_1 === '並立助詞'){
                                 secondFlag++
                             }
                         })
                     }
 
-                    if (secondFlag == 0){
+                    if (secondFlag === 0){
                         const ruleError = new RuleError("「など」「等」を使う場合は、2つ以上の具体例を挙げます。",{
                             index:position - 1 
                         });
